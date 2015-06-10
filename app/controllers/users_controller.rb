0e8ff764ebbf15ef2740f66e792d8b0a6403f58c
Scott Williams <williamsjs1224@gmail.com>
2015-06-09 20:17:20 -0400

    only allow user to update their own info in user_index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @user = User.find_by_id(session[:user_id])
   end
 
   # GET /users/1
